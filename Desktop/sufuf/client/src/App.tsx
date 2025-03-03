00a9cf2f546bfd21193b140345696af3bf0367b8
Rachid Azrou <rachid@Rachids-MacBook-Pro.local>
2025-03-03 13:43:39 +0100

    h

---
+++ Desktop/sufuf/client/src/App.tsx
@@ -0,0 +1,11 @@
+import { SufufPage } from './pages/sufuf';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-pattern">
+      <SufufPage />
+    </div>
+  );
+}
+
+export default App;
